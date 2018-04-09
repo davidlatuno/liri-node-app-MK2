4b2a1f87eb076f02cc1668fd0c043689b7e921e1
davidlatuno <dslatuno@gmail.com>
2018-04-08 19:45:17 -0700

    Finished dwis functionality

--- liri.js
+++ liri.js
@@ -332,11 +332,11 @@ function dwis() {
         // Run through appropriate function
         switch (command) {
             case "spotify-this-song":
-                userSpotify();
+                randomSpotify();
                 break;
 
             case "movie-this":
-                userMovie();
+                randomMovie();
                 break;
 
             case "my-tweets":
@@ -348,4 +348,158 @@ function dwis() {
                 break;
         }
     })
+}
+
+// OMDB Request for dwis function
+function randomMovie() {
+    // OMDB url
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchName + "&y=&plot=short&apikey=trilogy";
+    // Start entry to log.txt
+    fs.appendFile("log.txt", "\n\nMovie Search:", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+    // Empty User Input
+    if (searchName === "") {
+        request("http://www.omdbapi.com/?t=Mr. Nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var result = JSON.parse(body);
+                // Print info to terminal
+                console.log("Title: " + result.Title);
+                console.log("Year: " + result.Year);
+                console.log(`${result.Ratings[0].Source}: ${result.Ratings[0].Value}`);
+                console.log(`${result.Ratings[1].Source}: ${result.Ratings[1].Value}`);
+                console.log("Country: " + result.Country);
+                console.log("Language(s): " + result.Language);
+                console.log("Plot: " + result.Plot);
+                console.log("Actors: " + result.Actors);
+                // Append info to log.txt
+                fs.appendFile("log.txt", `\n\nTitle: ${result.Title}\nYear: ${result.Year}\n${result.Ratings[0].Source}: ${result.Ratings[0].Value}\n${result.Ratings[1].Source}: ${result.Ratings[1].Value}\nCountry: ${result.Country}\nLanguage(s): ${result.Language}\nPlot: ${result.Plot}\nActors: ${result.Actors}`, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            }
+            return new Promise(function (resolve, reject) {
+                resolve(1);
+            })
+            // Wait for api call to finish then ask user for another task
+                .then(function (result) {
+                    userSays()
+                })
+        })
+    } else {
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                // User Validation
+                if (JSON.parse(body).Error === "Movie not found!") {
+                    console.log("Movie not found");
+                    fs.appendFile("log.txt", "\n\nMovie not found!", function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                } else {
+                    var result = JSON.parse(body);
+                    // Print info to terminal
+                    console.log("Title: " + result.Title);
+                    console.log("Year: " + result.Year);
+                    console.log(`${result.Ratings[0].Source}: ${result.Ratings[0].Value}`);
+                    console.log(`${result.Ratings[1].Source}: ${result.Ratings[1].Value}`);
+                    console.log("Country: " + result.Country);
+                    console.log("Language(s): " + result.Language);
+                    console.log("Plot: " + result.Plot);
+                    console.log("Actors: " + result.Actors);
+                    // Append info to log.txt
+                    fs.appendFile("log.txt", `\n\nTitle: ${result.Title}\nYear: ${result.Year}\n${result.Ratings[0].Source}: ${result.Ratings[0].Value}\n${result.Ratings[1].Source}: ${result.Ratings[1].Value}\nCountry: ${result.Country}\nLanguage(s): ${result.Language}\nPlot: ${result.Plot}\nActors: ${result.Actors}`, function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                }
+            } else {
+                console.log("Something went wrong");
+            }
+            return new Promise(function (resolve, reject) {
+                resolve(1);
+            })
+            // Wait for api call to finish then ask user for another task
+                .then(function (result) {
+                    userSays()
+                })
+        })
+    }
+}
+
+// Spotify Request for dwis function
+function randomSpotify() {
+    // Start log.txt entry
+    fs.appendFile("log.txt", "\n\nSpotify Search:", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+    // Empty User input
+    if (searchName === "") {
+        spotify
+            .request('https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc')
+            .then(function (data) {
+                // Print info to terminal
+                console.log("Artist(s): " + data.artists[0].name);
+                console.log("Song Name: " + data.name);
+                console.log("Preview Link: " + data.external_urls.spotify);
+                console.log("Album from: " + data.album.name);
+                // Append info to log.txt
+                fs.appendFile("log.txt", `\n\nArtists(s): ${data.artists[0].name}\nSong Name: ${data.name}\nPreview Link: ${data.external_urls.spotify}\nAlbum from: ${data.album.name}`, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            })
+            .catch(function (err) {
+                console.error('Error occurred: ' + err);
+            })
+            .then(function() {
+                userSays();
+            })
+    } else {
+        // Handle User Input
+        spotify.search({ type: 'track', query: searchName }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            // User validation
+            if (data.tracks.total === 0) {
+                console.log("Sorry, could not find any songs matching " + searchName);
+                fs.appendFile("log.txt", `\n\nSorry, could not find any songs matching ${searchName}`, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            } else {
+                for (var i = 0; i < data.tracks.items.length; i++) {
+                    // Print info to terminal
+                    console.log("Artist(s): " + data.tracks.items[i].artists[0].name);
+                    console.log("Song Name: " + data.tracks.items[i].name);
+                    console.log("Preview Link: " + data.tracks.items[i].external_urls.spotify);
+                    console.log("Album from: " + data.tracks.items[i].album.name);
+                    console.log("");
+                    // Append info to log.txt
+                    fs.appendFile("log.txt", `\n\nArtists(s): ${data.tracks.items[i].artists[0].name}\nSong Name: ${data.tracks.items[i].name}\nPreview Link: ${data.tracks.items[i].external_urls.spotify}\nAlbum from: ${data.tracks.items[i].album.name}`, function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                }
+            }
+            return new Promise(function (resolve, reject) {
+                resolve(1);
+            })
+            // Wait for api call to finish then ask user for another task
+                .then(function (result) {
+                    userSays()
+                })
+        });
+    }
 }
